34d6dd6e80d89a0906086be4647b955d62c1279b
FaizBastomi <faizbastomy3@gmail.com>
2023-02-08 21:58:00 +0700

    RecyclerView ??

--- app/src/main/java/com/faiz/app_layouting/MainActivity.kt
+++ app/src/main/java/com/faiz/app_layouting/MainActivity.kt
@@ -2,8 +2,11 @@ package com.faiz.app_layouting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    val kucing = arrayOf("Persia", "Ras", "Garong")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
