4c75af93294db670b0b8fa7499408cdb58dc69d1
FaizBastomi <faizbastomy3@gmail.com>
2023-02-20 20:12:23 +0700

    unused dataNews update Glide

--- app/src/main/java/com/bootcamp/tugas3_bootcampidn/MainActivity.kt
+++ app/src/main/java/com/bootcamp/tugas3_bootcampidn/MainActivity.kt
@@ -14,7 +14,6 @@ import retrofit2.Callback
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    val dataNews = listOf<ArticlesItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
